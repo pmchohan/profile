fb3b3c6762bd3c9379e435a214e092fc406a424a
Abdullah Faisal <abdullahchohan9919@gmail.com>
2025-01-30 22:24:08 +0500

    minor

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -6,7 +6,7 @@ const NotFound: React.FC = () => {
       <br></br>
       <h1>404 - Page Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
-      <p>Go to <u><b><a href="/">Homepage</a></b></u></p>
+      <p>Go to <u><b><a href="/profile">Homepage</a></b></u></p>
     </div>
   );
 };
